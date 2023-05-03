aee27a18ae17f5c2c9df8b514c4849f40a007a3c
Finn Innes <finn123890@gmail.com>
2023-05-03 14:34:11 +1200

    files can now be imported by clicking 'Choose'

--- src/components/DropArea.js
+++ src/components/DropArea.js
@@ -11,6 +11,17 @@ const DropArea = ({ files, setFiles }) => {
 		setHovering(false);
   }
 
+  const handleChoose = (evt) => {
+    let input = document.createElement("input");
+    input.type = "file";
+    input.setAttribute("multiple", "");
+    input.onchange = (evt) => { 
+      const newFiles = Array.from(evt.target.files);
+      setFiles([...files, ...newFiles]); 
+    }
+    input.click();
+  }
+
   const handleDragEnter = (evt) => {
     evt.preventDefault();
 		setHovering(true);
@@ -34,7 +45,7 @@ const DropArea = ({ files, setFiles }) => {
 			onDragOver = { handleDragOver} 
 			onDragEnter={ handleDragEnter } 
 			onDragLeave={ handleDragLeave } 
-		><span><a>Choose</a> a file or drag it here</span></div>
+		><span><a onClick={ handleChoose }>Choose</a> a file or drag it here</span></div>
   );
 }
 
